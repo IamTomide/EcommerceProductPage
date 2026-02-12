2f215dfa086e32568479f3d648df28dc83304bd4
Olaleye Oluwatomide <paulinaebunoluwa@gmail.com>
2026-02-12 10:49:10 +0100

    Moved Lighbox implementation from productInfo component to productgallery component

--- src/components/Product/ProductGallery.jsx
+++ src/components/Product/ProductGallery.jsx
@@ -1,6 +1,7 @@
-import { useState} from 'react'
-import { Navigation, Thumbs, EffectFlip } from 'swiper/modules';
+import { useState, useEffect } from 'react'
+import { Navigation, Thumbs } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import Lightbox from '../Lightbox/Lightbox';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -10,47 +11,83 @@ import productData from '../../data/productsData'
 import './App.css'
 
 
-function ProductGallery({ isLightbox, setIsLightbox, setLightboxStartIndex, isMobile }) {
+
+function ProductGallery() {
     const [thumbsSwiper, setThumbsSwiper] = useState(null);
-    const productImages = productData[0].images
-    const imagesThumbs = productData[0].images
+    const productImages = productData[0].images;
+    const imagesThumbs = productData[0].images;
+    const [lightboxStartIndex, setLightboxStartIndex] = useState(0);
+    const [isLightbox, setIsLightbox] = useState(false);
+    const [lighboxSwiper, setLightboxSwiper] = useState(null);
+    const [isMobile, setIsMobile] = useState(() => {
+            return window.matchMedia("(max-width: 560px)").matches;
+        });
+    
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 560px)");
+        setIsMobile(mediaQuery.matches);
+
+        const handler = (e) => setIsMobile(e.matches);
+        mediaQuery.addEventListener("change", handler);
+
+        return () => mediaQuery.removeEventListener("change", handler);
+    }, []);
+
+
+    useEffect(() => {
+        if (!isLightbox) {
+            setLightboxSwiper(null);
+        }
+    }, [isLightbox]);
+
+    useEffect(() => {
+        if (isMobile && isLightbox) {
+            setIsLightbox(false);
+            setLightboxSwiper(null);
+        }
+    }, [isMobile, isLightbox]);
+
 
     return (
-         <section className="prodgallery">
-            <Swiper
-                modules={[Navigation, Thumbs]}
-                spaceBetween={50}
-                slidesPerView={1}
-                navigation={isMobile && !isLightbox}
-                thumbs={isMobile ? undefined : { swiper: thumbsSwiper }}
-                >
-                    {productImages.map((image, index)=> {
-                        return <SwiperSlide key={index}><img src={image} alt="product image" className="prod" onClick={() => {
-                            if (isMobile) {
-                                setIsLightbox(false);
-                            } else {
-                                setLightboxStartIndex(index);
-                                setIsLightbox(true);
-                            }
-                        }}/></SwiperSlide>
-                    })}
-            </Swiper>
+        <>
+            <section className="prodgallery">
+                <Swiper
+                    modules={[Navigation, Thumbs]}
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    navigation={isMobile && !isLightbox}
+                    thumbs={isMobile ? undefined : { swiper: thumbsSwiper }}
+                    >
+                        {productImages.map((image, index)=> {
+                            return <SwiperSlide key={index}><img src={image} alt="product image" className="prod" onClick={() => {
+                                if (isMobile) {
+                                    setIsLightbox(false);
+                                } else {
+                                    setLightboxStartIndex(index);
+                                    setIsLightbox(true);
+                                }
+                            }}/></SwiperSlide>
+                        })}
+                </Swiper>
 
-            {!isMobile && <Swiper
-                modules={[Thumbs]}
-                watchSlidesProgress
-                onSwiper={setThumbsSwiper}
-                className="thumbnail"
-                slidesPerView={4}
-                spaceBetween={10} 
-                >
-                    {imagesThumbs.map((image, index)=> {
-                        return <SwiperSlide className='thumbnailwrapper' key={index}><img src={image} alt="product image" className="imgthumbnail" /></SwiperSlide> 
-                    }
-                )}
-            </Swiper>}
+                {!isMobile && <Swiper
+                    modules={[Thumbs]}
+                    watchSlidesProgress
+                    onSwiper={setThumbsSwiper}
+                    className="thumbnail"
+                    slidesPerView={4}
+                    spaceBetween={10} 
+                    >
+                        {imagesThumbs.map((image, index)=> {
+                            return <SwiperSlide className='thumbnailwrapper' key={index}><img src={image} alt="product image" className="imgthumbnail" /></SwiperSlide> 
+                        }
+                    )}
+                </Swiper>}
 
-        </section>
+            </section>
+            {isLightbox && <Lightbox lighboxSwiper={lighboxSwiper} setLightboxSwiper={setLightboxSwiper} StartIndex={lightboxStartIndex} setIsLightbox={setIsLightbox} isLightbox={isLightbox}/>}
+            
+        </>
     )
 }
 
--- src/components/Product/ProductInfo.jsx
+++ src/components/Product/ProductInfo.jsx
@@ -1,48 +1,14 @@
-import Lightbox from '../Lightbox/Lightbox';
 import './App.css'
 import ProductDetails from './ProductDetails'
 import ProductGallery from './ProductGallery'
-import { useState, useEffect } from 'react'
 
 function ProductInfo() {
-    const [lightboxStartIndex, setLightboxStartIndex] = useState(0);
-    const [isLightbox, setIsLightbox] = useState(false);
-    const [lighboxSwiper, setLightboxSwiper] = useState(null);
-    const [isMobile, setIsMobile] = useState(() => {
-            return window.matchMedia("(max-width: 560px)").matches;
-        });
     
-    useEffect(() => {
-        const mediaQuery = window.matchMedia("(max-width: 560px)");
-        setIsMobile(mediaQuery.matches);
-
-        const handler = (e) => setIsMobile(e.matches);
-        mediaQuery.addEventListener("change", handler);
-
-        return () => mediaQuery.removeEventListener("change", handler);
-    }, []);
-
-
-    useEffect(() => {
-        if (!isLightbox) {
-            setLightboxSwiper(null);
-        }
-    }, [isLightbox]);
-
-    useEffect(() => {
-        if (isMobile && isLightbox) {
-            setIsLightbox(false);
-            setLightboxSwiper(null);
-        }
-    }, [isMobile, isLightbox]);
-
     return (
         <div className="mainstyle">
             <main>
-                <ProductGallery isMobile
-                ={isMobile} isLightbox={isLightbox} setIsLightbox={setIsLightbox} setLightboxStartIndex={setLightboxStartIndex}/>
+                <ProductGallery />
                 <ProductDetails />
-                {isLightbox && <Lightbox lighboxSwiper={lighboxSwiper} setLightboxSwiper={setLightboxSwiper} StartIndex={lightboxStartIndex} setIsLightbox={setIsLightbox} isLightbox={isLightbox}/>}
             </main>
         </div>
     )
